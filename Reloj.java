9e8dd5cf641dd8d4fc8b2523523b6cb91df6a8af
Deviyue <72820262+Deviyue@users.noreply.github.com>
2020-10-19 16:31:42 +0200

    Add files via upload
    actualizo reloj y intervalo aplicando observer y observable

---
+++ Reloj.java
@@ -0,0 +1,45 @@
+package TimeTracker;
+
+import java.util.Observable;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.time.LocalDateTime;
+//import java.lang.Object
+
+//Esta clase se encargará de acceder a la hora del dispositivo e ir actualizando a sus observadores
+class Reloj extends Observable {
+  Reloj observable = new Reloj();
+  private int periodo;
+  private Timer timer;
+  private static Reloj uniqueInstance = null;
+
+  //Constructor donde inicializamos el periodo mediante el cual irá actualizando la hora a los observadores.
+  private Reloj() {
+    this.periodo = 2;
+  }
+  
+  //Método que devuelve la instancia del reloj para asegurar que solo hay un reloj corriendo para todos los observadores. 
+  public static Reloj getInstance() {
+    if(uniqueInstance == null) {
+      return new Reloj();
+    } else {
+      return uniqueInstance;
+    }    
+  }
+  
+  //Función que actualiza la hora y notifica a los observadores.
+  public void start() {
+    Timer timer = new Timer(); 
+    TimerTask task = new TimerTask() {
+      public void run() {
+        Notify(); //Función a llamar cada 2" mediante scheduleAtFixedRate.
+      }
+    };
+    timer.scheduleAtFixedRate(task, 0, 1000 * periodo); //Método que repite la tarea que se pasa como primer argumento cada 1000*periodo = 2".
+  }
+  public void Notify(){
+    LocalDateTime hora = LocalDateTime.now(); //Guarda la hora actual del sistema.
+    notifyObservers(hora); //Notifica a los observadores y les envia el objeto del reloj.
+  }
+
+}
